1a06e908de9b216f841dff50f1a886384ba19b2f
Moleboheng Mahahle <molebohengmahahle@gmail.com>
2025-10-24 16:30:21 +0200

    Update RetailSalesCode.sql

--- RetailSalesCode.sql
+++ RetailSalesCode.sql
@@ -1,27 +1,31 @@
--- EDA
+--INITIAL DATA EXPLORATION
 SELECT *
 FROM RETAIL.PUBLIC.SALES;
 
+--PRODUCT CATEGORY ANALYSIS
 SELECT DISTINCT PRODUCT_CATEGORY
 FROM RETAIL.PUBLIC.SALES;
 
+--DATE RANGE ANALYSIS
 SELECT MIN(DATE) AS earliest_date
 FROM RETAIL.PUBLIC.SALES;
 
 SELECT MAX(DATE) AS latest_date
 FROM RETAIL.PUBLIC.SALES;
 
+--CUSTOMER AGE ANALYSIS
 SELECT MIN(Age) AS youngest_cust
 FROM RETAIL.PUBLIC.SALES;
 
 SELECT MAX(Age) AS oldest_cust
 FROM RETAIL.PUBLIC.SALES;
 
--- PROCESSING
+--MAIN PROCESSING
 SELECT
 COUNT(DISTINCT transaction_id) AS number_of_sales,
 COUNT(DISTINCT customer_id) AS number_of_customers,
 
+--TIME DIMENSIONS
 DAYNAME(date) AS day_name,
 MONTHNAME(date) AS month_name,
 DAYOFMONTH(date) AS day_of_month,
@@ -32,10 +36,12 @@ product_category,
 quantity,
 price_per_unit,
 total_amount,
-
+  
+--AGGREGATES
 SUM(IFNULL(total_amount,0)) AS total_revenue,
 SUM(IFNULL(quantity,0)) AS total_quantity,
 
+--CREATING AGE BUCKETS
 CASE 
 WHEN age BETWEEN 18 AND 35 THEN 'Youth(18-35)'
 WHEN age BETWEEN 36 AND 59 THEN 'Adult(36-59)'
